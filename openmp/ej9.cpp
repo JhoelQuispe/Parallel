3f51c3605c8965dfecb8f8a4919cf620cfe32d14
Jhoel <jhoel.quispe.123@gmail.com>
2015-11-24 08:30:12 -0500

    5.9..

--- openmp/ej9.cpp
+++ openmp/ej9.cpp
@@ -1,115 +1,95 @@
-#include <iostream>
-#include <cstdio>
+/* File:    omp_trap3.c
+ * Purpose: Estimate definite integral (or area under curve) using the
+ *          trapezoidal rule.  This version uses a parallel for directive
+ *
+ * Input:   a, b, n
+ * Output:  estimate of integral from a to b of f(x)
+ *          using n trapezoids.
+ *
+ * Compile: gcc -g -Wall -fopenmp -o omp_trap3 omp_trap3.c
+ * Usage:   ./omp_trap3 <number of threads>
+ *
+ * Notes:   
+ *   1.  The function f(x) is hardwired.
+ *   2.  In this version, it's not necessary for n to be
+ *       evenly divisible by thread_count.
+ *
+ * IPP:  Section 5.5 (pp. 224 and ff.)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <omp.h>
-#include <cstdlib>
 
-#include <vector>
+void Usage(char* prog_name);
+double f(double x);    /* Function we're integrating */
+double Trap(double a, double b, int n, int thread_count);
 
-using namespace std;
+int main(int argc, char* argv[]) {
+   double  global_result = 0.0;  /* Store result in global_result */
+   double  a, b;                 /* Left and right endpoints      */
+   int     n;                    /* Total number of trapezoids    */
+   int     thread_count;
 
-// double trap(){
+   if (argc != 2) Usage(argv[0]);
+   thread_count = strtol(argv[1], NULL, 10);
+   printf("Enter a, b, and n\n");
+   scanf("%lf %lf %d", &a, &b, &n);
 
-// 	cout<<"calling trap rule" << endl;
-// 	double left;
-// 	double right;
+   global_result = Trap(a, b, n, thread_count);
 
-// 	int trap_count;
-// 	double base_len;
-// 	double estimate, x;
+   printf("With n = %d trapezoids, our estimate\n", n);
+   printf("of the integral from %f to %f = %.14e\n",
+      a, b, global_result);
+   return 0;
+}  /* main */
 
+/*--------------------------------------------------------------------
+ * Function:    Usage
+ * Purpose:     Print command line for function and terminate
+ * In arg:      prog_name
+ */
+void Usage(char* prog_name) {
 
-// }
+   fprintf(stderr, "usage: %s <number of threads>\n", prog_name);
+   exit(0);
+}  /* Usage */
 
-double f(double x){
-	return x*2;
-}
+/*------------------------------------------------------------------
+ * Function:    f
+ * Purpose:     Compute value of function to be integrated
+ * Input arg:   x
+ * Return val:  f(x)
+ */
+double f(double x) {
+   double return_val;
 
+   return_val = x*x;
+   return return_val;
+}  /* f */
 
-void Trap(double a, double b, int n , double* global_result_p){
+/*------------------------------------------------------------------
+ * Function:    Trap
+ * Purpose:     Use trapezoidal rule to estimate definite integral
+ * Input args:  
+ *    a: left endpoint
+ *    b: right endpoint
+ *    n: number of trapezoids
+ * Return val:
+ *    approx:  estimate of integral from a to b of f(x)
+ */
+double Trap(double a, double b, int n, int thread_count) {
+   double  h, approx;
+   int  i;
 
-	double h, x, my_result;
-	double local_a, local_b;
+   h = (b-a)/n; 
+   approx = (f(a) + f(b))/2.0; 
+#  pragma omp parallel for num_threads(thread_count) \
+      reduction(+: approx)
+   for (i = 1; i <= n-1; i++)
+     approx += f(a + i*h);
+   approx = h*approx; 
 
-	int local_n;
-	int my_rank = omp_get_thread_num();
-	int thread_count = omp_get_num_threads();
-
-	h = (b-a)/n;
-	local_n = n/thread_count;
-	local_a = a + my_rank*local_n*h;
-	local_b = local_a + local_n*h;
-
-	my_result = (f(local_a) + f(local_b))/2.0;
-	for(int i = 1; i <= local_n-1; ++i){
-		x = local_a + i*h;
-		my_result += f(x);
-	}
-
-	my_result = my_result*h;
-
-	#pragma omp critical
-	*global_result_p += my_result;
-
-
-}
-
-void hello(void){
-
-	int my_rank = omp_get_thread_num();
-	int thread_count = omp_get_num_threads();
-
-	printf("Hello from thread %d of %d\n", my_rank, thread_count);
-}
-
-
-
-
-int main(int argc, char* argv[]){
-
-	double global_result = 0.0;
-	double a,b;
-	int n;
-
-	int thread_count = strtol(argv[1] , NULL , 10);
-	
-	// printf("Enter a, b and n\n");
-	// scanf("%lf %lf %d", &a, &b, &n);
-
-	// #pragma omp parallel num_threads(thread_count)
-	// Trap(a, b, n, &global_result);
-
-	// printf("With n = %d trapezoids, the estimate\n", n);
-	// printf("of the integral from %f to %f = %f\n", a, b, global_result );
-
-
-	// #pragma omp parallel num_threads(thread_count)
-	// hello();
-
-	int elements = 4;
-
-	// vector <int>  v [thread_count];
-
-
-	// cout<< "antes de llenar" << endl;
-
-
-	#pragma omp parallel for num_threads(thread_count) 
-	for (int i = 0; i < elements; ++i)
-	{
-		int my_rank = omp_get_thread_num();
-		printf("iteration %d thread -> %d\n", i, my_rank);
-		// v[my_rank].push_back(i);
-	}
-
-	// cout<< "despues de llenar" << endl;
-
-	// for(int i = 0; i < thread_count; ++i){
-	// 	for (int j = 0; j < elements; ++j) {
-	// 		cout << v[i][j] << " ";
-	// 	}	
-	// 	cout << endl;
-	// }
-
-	return 0;
-		
-}
+   return approx;
+}  /* Trap */
